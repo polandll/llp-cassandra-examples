ccd8b46b1c582bf3d73ca802cfdb6a3f8ddfa0e9
Lorina Poland <lorina@datastax.com>
2015-01-27 15:04:11 -0800

    More file cleanup, added variables to check.sh to switch between VM and right scale

--- PRACTICUM/check.sh
+++ PRACTICUM/check.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-../../dsc-cassandra-2.1.0/bin/cqlsh -f p_catfood_check.cql
-grep BRAND2 output.txt > output.clean
-if [ -s output.clean ]
+CQLSH="../../dsc-cassandra-2.1.0/bin/cqlsh"
+# RS_CQLSH="cqlsh node0"
+CQLSH_FLAGS="-f"
+
+P_CATFOOD_CHECK="p_catfood_check.cql"
+# RS_P_CATFOOD_CHECK="p_catfood_check_rightscale.cql"
+
+OUTPUT_TEXT="output.txt"
+OUTPUT_CLEAN="output.clean"
+
+FIRST_FIELD="BRAND2"
+SECOND_FIELD="BRAND4"
+
+$CQLSH $CQLSH_FLAGS $P_CATFOOD_CHECK
+# $RS_CQLSH $CQLSH_FLAGS $RS_P_CATFOOD_CHECK
+grep $FIRST_FIELD $OUTPUT_TEXT > $OUTPUT_CLEAN 
+if [ -s $OUTPUT_CLEAN ]
 then 
   echo "RIGHT" 
 else 
