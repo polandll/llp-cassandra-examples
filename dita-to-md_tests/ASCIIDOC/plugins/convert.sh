7fd70813aa0687aad5c4b3ff56fbbf165554e626
polandll <lorinapoland@gmail.com>
2020-05-15 16:24:40 -0700

    asciidoc tests

---
+++ dita-to-md_tests/ASCIIDOC/plugins/convert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for f in ~/CLONES/cassandra/doc/source/plugins/*.rst
+do
+    filename=$(basename "$f")
+    filename="${filename%.*}"
+    echo "Processing $filename"
+    pandoc "$f" -f rst -t asciidoc -s -o "$filename.adoc"
+done
