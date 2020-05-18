b4051521e15de6bf67de7470e9799915613115b7
polandll <lorinapoland@gmail.com>
2020-05-18 14:49:17 -0700

    rearrange

--- dita-to-md_tests/ASCIIDOC/modules/ROOT/architecture/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for f in ~/CLONES/cassandra/doc/source/architecture/*.rst
-do
-    filename=$(basename "$f")
-    filename="${filename%.*}"
-    echo "Processing $filename"
-    pandoc "$f" -f rst -t asciidoc -s -o "$filename.adoc"
-done
